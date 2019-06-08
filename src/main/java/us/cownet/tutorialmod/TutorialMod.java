cd0d0ec846b5ef84795ce4e1d3d58183ec737b8b
fitzpatricksrus <junk@fitzpatircksr.us>
2019-06-08 10:51:22 -0700

    First checkin

---
+++ src/main/java/us/cownet/tutorialmod/TutorialMod.java
@@ -0,0 +1,38 @@
+package us.cownet.tutorialmod;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = TutorialMod.MODID, name = TutorialMod.NAME, version = TutorialMod.VERSION)
+public class TutorialMod {
+	public static final String MODID = "tutorialmod";
+	public static final String NAME = "Cownet Tutorial Mod";
+	public static final String VERSION = "1.0";
+
+	private static Logger logger;
+
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event)
+	{
+		logger = event.getModLog();
+		logger.info(MODID + ":preInit");
+	}
+
+	@EventHandler
+	public void init(FMLInitializationEvent event)
+	{
+		// some example code
+		logger.info(MODID + ":init");
+		logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+	}
+
+	@EventHandler
+	public void postInit(FMLPostInitializationEvent event) {
+		logger.info(MODID + ":postInit");
+	}
+}
