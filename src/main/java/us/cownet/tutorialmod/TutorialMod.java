06772866fef7e9832da95bbdfe7c78f96aa0d2c4
fitzpatricksrus <junk@fitzpatircksr.us>
2019-06-08 11:00:06 -0700

    Added @Mod.instance to main class so we can get to the main instance whenever we need it.

--- src/main/java/us/cownet/tutorialmod/TutorialMod.java
+++ src/main/java/us/cownet/tutorialmod/TutorialMod.java
@@ -14,6 +14,9 @@ public class TutorialMod {
 	public static final String NAME = "Cownet Tutorial Mod";
 	public static final String VERSION = "1.0";
 
+	@Mod.Instance
+	public static TutorialMod instance;
+
 	private static Logger logger;
 
 	@EventHandler
